a0d1ef3c81998cbba0bfca4645d1920c7bdf5294
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-09-24 11:27:00 +1200

    background images added

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 import './menu-item.style.scss'
 
-export const MenuItem = ({title}) => (
+export const MenuItem = ({title,imageUrl,size}) => (
       
-           <div className="menu-item">
+           <div style={{
+             backgroundImage : `url(${imageUrl})`
+           }} 
+            className={`${size} menu-item` }>
              <div className="content">
                 <h1 className="title">{title}</h1>
                <span className="subtitle">SHOP NOW</span>
