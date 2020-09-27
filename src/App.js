752f4b338268985b448208fee419b54c60368791
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-09-28 12:29:04 +1300

    preview shop component create and connected

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/homepage/homepage'
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+
+import HomePage from './pages/homepage/homepage'
+import ShopPage from './pages/shop/shop'
+
 
 
 function App() {
@@ -16,7 +14,7 @@ function App() {
     <div>
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/shop/hats' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
 
       
       </Switch>
