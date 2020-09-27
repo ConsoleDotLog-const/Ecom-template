a982e7452be68c808c9137784a0c6d4fc40012f0
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-09-27 20:37:42 +1300

    updated browserrouteing

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./directory.styles.scss";
 
-import { MenuItem } from "../menu-item/menu-item";
+import MenuItem from "../menu-item/menu-item";
 
 class Directory extends React.Component {
   constructor() {
@@ -49,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({  id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
