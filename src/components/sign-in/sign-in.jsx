a492d46f2ab8668fd617d2043f830e43de74dfaa
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-10-07 12:21:08 +1300

    styling in sign in and app.css updated

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -48,11 +48,12 @@ class SignIn extends React.Component{
                             value={this.state.password} 
                             label="password"
                             required />
-                    
+                    <div className="buttons">
                     <CustomButton type="submit"> Sign In </CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn >  
                     Sign In with Google 
                     </CustomButton>
+                    </div>
                 </form>    
             </div>
         )
