e51fcd108e2874b8ff855a0ef942a03fc15a8303
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-10-08 12:35:09 +1300

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/sign-in-up/sign-in-up.jsx
+++ src/components/sign-in-up/sign-in-up.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import SignIn from '../sign-in/sign-in'
+import SignUp from '../sign-up/sign-up'
 
 import './sign-in-up.styles.scss'
 
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         <SignIn/>
+        <SignUp/>
     </div>
 )
 
