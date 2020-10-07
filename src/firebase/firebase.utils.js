5326ef55893930e9bb368c2076c3c6a4f7cf4d02
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-10-08 11:37:23 +1300

    firestore connected and recieves data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,36 +3,47 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyChMt0g0MlTL1VgLpXO_eaR8-UB1S_LzzM",
-    authDomain: "e-com-template-8fc9d.firebaseapp.com",
-    databaseURL: "https://e-com-template-8fc9d.firebaseio.com",
-    projectId: "e-com-template-8fc9d",
-    storageBucket: "e-com-template-8fc9d.appspot.com",
-    messagingSenderId: "993954444320",
-    appId: "1:993954444320:web:fc1c396cf269fbe148b845"
-  }
+  apiKey: "AIzaSyChMt0g0MlTL1VgLpXO_eaR8-UB1S_LzzM",
+  authDomain: "e-com-template-8fc9d.firebaseapp.com",
+  databaseURL: "https://e-com-template-8fc9d.firebaseio.com",
+  projectId: "e-com-template-8fc9d",
+  storageBucket: "e-com-template-8fc9d.appspot.com",
+  messagingSenderId: "993954444320",
+  appId: "1:993954444320:web:fc1c396cf269fbe148b845"
+};
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-  
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-  
-    const snapShot = await userRef.get();
+firebase.initializeApp(config);
 
-    if(!snapShot.exists){
-      
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
-    
   }
 
-  firebase.initializeApp(config);
+  return userRef;
+};
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore()
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt : 'select_account'})
-  export const signInWithGoogle = () => auth.signInWithPopup(provider)
-
-  export default firebase;
+export default firebase;
