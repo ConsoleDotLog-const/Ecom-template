394644de5801f73ca935c569ab79dd6bcd4a5de6
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-10-14 10:51:23 +1300

    checkout page contains styling and total selector

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import { withRouter } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { withRouter } from "react-router-dom";
 
-import CartItem from '../cart-item/cart-item'
-import CustomButton from '../custom-bottom/custom-button'
-import { selectCartItems } from '../../redux/cart/cart.selectors'
+import CartItem from "../cart-item/cart-item";
+import CustomButton from "../custom-bottom/custom-button";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
- import './cart-dropdown.styles.scss'
+import "./cart-dropdown.styles.scss";
 
- const CartDropdown = ({ cartItems, history }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
-      {
-        cartItems.length ? (
-      cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))
-         ) :(
+const CartDropdown = ({ cartItems, history }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
         <span className="empty-message">Your cart is empty</span>
-         )}
+      )}
     </div>
-    <CustomButton onClick={()=>history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push("/checkout")}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector ({
- cartItems: selectCartItems
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
-  
