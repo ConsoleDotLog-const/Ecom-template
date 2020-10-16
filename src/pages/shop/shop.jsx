b8077a6329248b3c801d92097b58429b8beb9587
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-10-16 18:03:50 +1300

    destructing bug for collectionsPage

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview'
+import CollectionsPage from '../collection/collection'
 
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => {
+  console.log(match)
+  return(
   <div className="shop-page">
-      <CollectionsOverview collections={collections}/>
+       <Route  exact path = {`${match.path}`} component = {CollectionsOverview} />
+       <Route  path={`${match.path}/:categoryId`} component={CollectionsPage}/>
   </div>
-);
+)};
 
 
 
